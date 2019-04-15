a7debd0fdd32646deb8c09df615d9554b40e21b4
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2019-04-15 09:55:41 +0200

    - quote values - use absolute path

--- manifests/value.pp
+++ manifests/value.pp
@@ -2,12 +2,12 @@ define sysctl::value(
   $value
 ){
   require sysctl
-  exec{"exec_sysctl_$name":
-    command => "sysctl $name=$value",
+  exec{ "exec_sysctl_$name":
+    command     => "/sbin/sysctl ${name}='${value}'",
     refreshonly => true,
   }
-  sysctl{$name:
-    val => $value,
+  sysctl{ $name:
+    val    => $value,
     notify => Exec["exec_sysctl_$name"],
   }
 }
